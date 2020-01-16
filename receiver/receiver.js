89c35b039e17b169614f9690dfc9f7533e5c5e42
nagamocha3000 <nagamocha@gmail.com>
2020-01-16 21:19:28 +0300

    add getting hash of file as it is received

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -1,6 +1,7 @@
 const net = require("net");
 const path = require("path");
 const fs = require("fs");
+const crypto = require("crypto");
 
 const filesDir = path.join(path.dirname(__filename), "files");
 
@@ -9,16 +10,26 @@ const senderConn = net.connect(9000);
 senderConn.on("connect", () => {
     let size = 0;
     let start = Date.now();
-    let fileWriteStream = fs.createWriteStream(
-        path.join(filesDir, `file_received-${Date.now()}.txt`)
-    );
+    let filePath = path.join(filesDir, `file_received-${Date.now()}.txt`);
+    let fileWriteStream = fs.createWriteStream(filePath);
+
     senderConn.on("data", chunk => {
         size += chunk.length;
         fileWriteStream.write(chunk);
     });
     senderConn.on("end", () => {
         let totalTimeMs = Date.now() - start;
-        console.log(`Received: ${size} bytes\nTime elapsed: ${totalTimeMs} ms`);
-        process.exit();
+        let shasum = crypto.createHash("sha256");
+        let fileStream = fs.createReadStream(filePath);
+        fileStream.on("data", data => {
+            shasum.update(data);
+        });
+        fileStream.on("end", () => {
+            let hash = shasum.digest("hex");
+            console.log(`Received:\t${size} bytes`);
+            console.log(`Time elapsed:\t${totalTimeMs} ms`);
+            console.log(`Hash:\t\t${hash}`);
+            process.exit();
+        });
     });
 });
