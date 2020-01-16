6901938e52dd7c0ab65e23428e127b9c9d54f0f1
nagamocha3000 <nagamocha@gmail.com>
2020-01-16 23:52:48 +0300

    add header parsing and file verification in receiver

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -10,22 +10,55 @@ const senderConn = net.connect(9000);
 senderConn.on("connect", () => {
     let size = 0,
         start = Date.now(),
-        filePath = path.join(filesDir, `file_received-${Date.now()}.txt`),
-        fileWriteStream = fs.createWriteStream(filePath),
-        shasum = crypto.createHash("sha256");
+        shasum = crypto.createHash("sha256"),
+        buffer = "",
+        state = "header",
+        fileWriteStream,
+        header = {},
+        recvErr = null;
 
-    senderConn.on("data", chunk => {
-        size += chunk.length;
-        fileWriteStream.write(chunk);
-        shasum.update(chunk);
+    senderConn.on("data", data => {
+        switch (state) {
+            case "header":
+                buffer += data;
+                let boundary = buffer.indexOf("\n");
+                if (boundary !== -1) {
+                    const headerJSON = buffer.substring(0, boundary);
+                    buffer = buffer.substring(boundary + 1);
+                    try {
+                        header = JSON.parse(headerJSON);
+                        let filePath = path.join(filesDir, header.fileName);
+                        size += Buffer.byteLength(buffer, "utf8");
+                        fileWriteStream = fs.createWriteStream(filePath);
+                        fileWriteStream.write(buffer);
+                        shasum.update(buffer);
+                        state = "file";
+                    } catch (err) {
+                        recvErr = err;
+                        senderConn.end();
+                    }
+                }
+                break;
+            case "file":
+                size += data.length;
+                fileWriteStream.write(data);
+                shasum.update(data);
+                break;
+        }
     });
 
     senderConn.on("end", () => {
         let totalTimeMs = Date.now() - start,
-            hash = shasum.digest("hex");
-        console.log(
-            `Received:\t${size} bytes\nTime elapsed:\t${totalTimeMs} ms\nHash:\t\t${hash}`
-        );
+            hash = shasum.digest("hex"),
+            verified = hash === header.fileHash;
+        if (recvErr === null) {
+            console.log(
+                `Received:\t${size}/${header.fileSize ||
+                    NaN} bytes\nTime elapsed:\t${totalTimeMs} ms\nVerified:\t${verified}\nHash:\t\t${hash}`
+            );
+        } else {
+            console.error(recvErr);
+        }
         process.exit();
     });
 });
